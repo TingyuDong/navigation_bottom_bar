edb0492d57458172f55fafaed78e1c92f8d784b3
Tingyu Dong <tingyu.dong@thoughtworks.com>
2023-02-23 13:22:36 +0800

    feat: handled return methods in nested fragments by another way

--- app/src/main/java/com/thoughtworks/myapplicationbottombar/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/myapplicationbottombar/MainActivity.kt
@@ -3,15 +3,21 @@ package com.thoughtworks.myapplicationbottombar
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.thoughtworks.myapplicationbottombar.`interface`.NestedFragment
 import com.thoughtworks.myapplicationbottombar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -31,6 +37,26 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener {
+            NavigationUI.onNavDestinationSelected(
+                it,
+                navController
+            )
+            true
+        }
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
     }
+
+    override fun onBackPressed() {
+        val primaryNavigationFragment =
+            navHostFragment.childFragmentManager.primaryNavigationFragment
+        if (primaryNavigationFragment is NestedFragment) {
+            primaryNavigationFragment.onBackPressed()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 
 }
--- app/src/main/java/com/thoughtworks/myapplicationbottombar/ui/notifications/NewsContainerFragment.kt
+++ app/src/main/java/com/thoughtworks/myapplicationbottombar/ui/notifications/NewsContainerFragment.kt
@@ -6,9 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.thoughtworks.myapplicationbottombar.`interface`.NestedFragment
 import com.thoughtworks.myapplicationbottombar.databinding.FragmentNotificationsBinding
 
-class NewsContainerFragment : Fragment() {
+class NewsContainerFragment : Fragment(), NestedFragment {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -32,5 +33,13 @@ class NewsContainerFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onBackPressed() {
+        if (childFragmentManager.backStackEntryCount > 0) {
+            childFragmentManager.popBackStack()
+        } else {
+            parentFragmentManager.popBackStack()
+        }
     }
 }
